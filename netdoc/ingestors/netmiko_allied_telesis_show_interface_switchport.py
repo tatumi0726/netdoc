849fc1ebef99b1693800fad32da911258e5fb58b
tatumi0726 <tatumi0726@gmail.com>
2024-02-19 23:38:41 +0900

    fix native_vlan

--- netdoc/ingestors/netmiko_allied_telesis_show_interface_switchport.py
+++ netdoc/ingestors/netmiko_allied_telesis_show_interface_switchport.py
@@ -16,9 +16,10 @@ def ingest(log):
         interface_name = item.get("interface")
         label = utils.normalize_interface_label(interface_name)
         mode = utils.normalize_interface_mode(item.get("mode"))
-        native_vlan = int(item.get("native_vlan")) if item.get("native_vlan") else None
-        #       native_vlan = item.get("access_vlan")
-        #       access_vlan = native_vlan
+        if item.get("native_vlan") == "None":
+            native_vlan = ""
+        else:
+            native_vlan = int(item.get("native_vlan")) if item.get("native_vlan") else None
         tagged_vlans = utils.normalize_vlan_list(item.get("trunking_vlans"))
 
         # Get or create Interface
